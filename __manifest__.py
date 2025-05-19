ab4c1923d65053a9d2c2470673f549b169348bd2
christianVillaPlayonSports <christian.villa@playonsports.com>
2025-05-19 08:41:51 -0400

    feat: Add approvals dependency to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "version": "15.0",
     "category": "Sale",
     "author": "Kritiam",
-    "depends": ["base", "mrp", "account", "web"],
+    "depends": ["base", "mrp", "account", "web", "approvals"],
     "data": [
         "security/ir.model.access.csv",
         "views/pagos_wizzard.xml",
